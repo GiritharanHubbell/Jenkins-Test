438d163e4a53a9b0b7f65882bd216ddc6ff7a595
EEE Common <eeecommon@hubbell.com>
2018-03-28 08:11:47 +0530

    Morning changes

--- Git Practice/ViewController.swift
+++ Git Practice/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("a b c d e f h i j k L")
+        print("a b c ")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
